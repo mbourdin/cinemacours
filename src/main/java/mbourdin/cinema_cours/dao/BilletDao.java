3fee7fc820bf999bff222224067c0d51dabcaa83
mbourdin <bourdin.maurice@gmail.com>
2019-01-08 21:01:39 +0100

    Commit du 8/1/19

--- src/main/java/mbourdin/cinema_cours/dao/BilletDao.java
+++ src/main/java/mbourdin/cinema_cours/dao/BilletDao.java
@@ -3,5 +3,8 @@ package mbourdin.cinema_cours.dao;
 import mbourdin.cinema_cours.model.Billet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface BilletDao extends CrudRepository<Billet,Long> {
+    Set<Billet> findAllByCommande_PayeAndSeanceId(Boolean paye,Long id);
 }
--- src/main/java/mbourdin/cinema_cours/dao/CommandeDao.java
+++ src/main/java/mbourdin/cinema_cours/dao/CommandeDao.java
@@ -1,7 +1,11 @@
 package mbourdin.cinema_cours.dao;
 
+import mbourdin.cinema_cours.model.Billet;
 import mbourdin.cinema_cours.model.Commande;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface CommandeDao extends CrudRepository<Commande,Long> {
+
 }
