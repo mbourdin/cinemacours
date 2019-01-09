c707053924fc72fcbe2522351e2d09ef5d2a5d60
mbourdin1983 <bourdin.maurice@gmail.com>
2019-01-09 12:36:02 +0100

    Commit du 9/1/19

--- src/main/java/mbourdin/cinema_cours/dao/CommandeDao.java
+++ src/main/java/mbourdin/cinema_cours/dao/CommandeDao.java
@@ -2,10 +2,12 @@ package mbourdin.cinema_cours.dao;
 
 import mbourdin.cinema_cours.model.Billet;
 import mbourdin.cinema_cours.model.Commande;
+import mbourdin.cinema_cours.model.Utilisateur;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface CommandeDao extends CrudRepository<Commande,Long> {
-
+    Commande findCommandeByPayeFalseAndUtilisateur(Utilisateur utilisateur);
 }
