8c0649c04e8742c617c0fa4811ec69c9f49bd4d5
mbourdin <bourdin.maurice@gmail.com>
2019-01-10 23:57:46 +0100

    Commit du 10/1/19

--- src/main/java/mbourdin/cinema_cours/dao/GenreDao.java
+++ src/main/java/mbourdin/cinema_cours/dao/GenreDao.java
@@ -3,9 +3,11 @@ package mbourdin.cinema_cours.dao;
 import mbourdin.cinema_cours.model.Genre;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Set;
 
 public interface GenreDao extends CrudRepository<Genre,Long> {
     @Override
     Set<Genre> findAll();
+    List<Genre> findAllByOrderByNameAsc();
 }
