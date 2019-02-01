5039fb203f2c94183f6616473d94a81f1c756b98
mbourdin1983 <bourdin.maurice@gmail.com>
2019-02-01 16:39:11 +0100

    commit du 31/01/19

--- src/main/java/mbourdin/cinema_cours/service/TmdbClient.java
+++ src/main/java/mbourdin/cinema_cours/service/TmdbClient.java
@@ -6,6 +6,8 @@ import mbourdin.cinema_cours.dao.PersonneDao;
 import mbourdin.cinema_cours.model.Film;
 import mbourdin.cinema_cours.model.Genre;
 import mbourdin.cinema_cours.model.Personne;
+import mbourdin.cinema_cours.model.Play;
+import org.checkerframework.checker.units.qual.A;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +16,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 
 
 @Component
 public class TmdbClient {
+    @Autowired
+    ImageManager imageManager;
     @Autowired
     FilmDao filmDao;
     @Autowired
@@ -46,6 +54,25 @@ public class TmdbClient {
         return value.substring(0, value.length()-1).substring(1);
     }
 
+    public void getImage(Personne personne)
+    {   String adresse="http://image.tmdb.org/t/p/w500"+personne.getPhotopath();
+        URL url;
+        try{
+            url=new URL(adresse);
+            InputStream is=url.openStream();
+            imageManager.savePhoto(personne,is);
+        }catch (Exception e){e.printStackTrace();}
+    }
+    public void getImage(Film film)
+    {   String adresse="http://image.tmdb.org/t/p/w500"+film.getAfficheNom();
+        URL url;
+        try{
+            url=new URL(adresse);
+            InputStream is=url.openStream();
+            imageManager.savePoster(film,is);
+        }catch (Exception e){e.printStackTrace();}
+    }
+
     public Personne getPersonByTmdbId(long id) throws Exception
     {   RestTemplate template = new RestTemplate();
         ResponseEntity<String> response;
@@ -57,9 +84,15 @@ public class TmdbClient {
         Personne importedPerson;
 
         //Long tmdbId=person.getLong("id");
-        String imagepath=person.getString("profile_path");
 
-        LocalDate annee=LocalDate.parse(person.getString("birthday"));
+        String imagepath=null;
+        if(!person.isNull("profile_path"))
+        {   imagepath = person.getString("profile_path");
+        }
+        LocalDate annee=null;
+        if(!person.isNull("birthday"))
+        {   annee=LocalDate.parse( person.getString("birthday"));
+        }
         String name=person.getString("name");
         Personne personne=personneDao.findByTmdbId(id);
         if(personne!=null)
@@ -71,6 +104,7 @@ public class TmdbClient {
             importedPerson.setNaissance(annee);
             importedPerson.setName(name);
             importedPerson.setPhotopath(imagepath);
+            importedPerson.setRoles(new ArrayList<Play>());
         }
         personneDao.save(importedPerson);
         return importedPerson;
@@ -102,12 +136,11 @@ public class TmdbClient {
         Optional<Film> filmOptional=filmDao.findByTmdbId(tmdbId);
         if(filmOptional.isPresent())
         {   importedFilm=filmOptional.get();
-            importedFilm.setAfficheNom(imagepath);
         }
         else {
             importedFilm = new Film();
             importedFilm.setGenres(new HashSet<Genre>());
-
+            importedFilm.setRoles(new ArrayList<Play>());
         }
         importedFilm.setTmdbId(tmdbId);
         importedFilm.setTitreOriginal(titreOriginal);
@@ -116,6 +149,9 @@ public class TmdbClient {
         importedFilm.setResume(resume);
         importedFilm.setNote(note);
         importedFilm.setDuree(duree);
+        importedFilm.setAfficheNom(imagepath);
+
+        getImage(importedFilm);// cela appelle la sauvegarde du film
         Genre importedgenre;
         for(int i = 0; i < genres.length(); i++){
             JSONObject genre =genres.getJSONObject(i);
@@ -135,7 +171,8 @@ public class TmdbClient {
 
 
         System.out.println(importedFilm);
-        filmDao.save(importedFilm);
+
+        //filmDao.save(importedFilm); la fonction getImage sauvegarde déja !
 
         System.out.println("--------\nRequetes restantes : "+stripBraces(response.getHeaders().get("x-ratelimit-remaining").toString()));
         reset = secondsBeforeReset(response.getHeaders().get("x-ratelimit-reset").toString());
@@ -146,12 +183,31 @@ public class TmdbClient {
         response = template.getForEntity(resourceCredit, String.class);
         JSONObject credit = new JSONObject(response.getBody());
 
-
+        Personne acteur;
+        Play play;
         JSONArray cast =credit.getJSONArray("cast");
         for (int i = 0; i < cast.length(); i++ ) {
             JSONObject role =cast.getJSONObject(i);
-            System.out.println(role.getString("name")+" joue "+ role.getString("character"));
+            String personnage=role.getString("character");
+            long actorId=role.getLong("id");
+            int order=role.getInt("order");
+            acteur=personneDao.findByTmdbId(actorId);
+            if(acteur==null)
+            {   acteur=getPersonByTmdbId(actorId);
+                getImage(acteur);
+            }
+            play=new Play();
+            play.setPersonne(acteur);
+            play.setFilm(importedFilm);
+            play.setNumero(order);
+            play.setNom(personnage);
+            play.addPlay();
+            //acteur.addFilm(importedFilm);
+            personneDao.save(acteur);
+            filmDao.save(importedFilm);
+
         }
+
         Personne realisateur=null;
         JSONArray crew=credit.getJSONArray("crew");
         for (int i = 0; i < crew.length(); i++ ) {
@@ -162,16 +218,19 @@ public class TmdbClient {
                 realisateur=personneDao.findByTmdbId(job.getLong("id"));
                 if(realisateur==null)
                 {   realisateur=getPersonByTmdbId(job.getLong("id"));
-
+                    getImage(realisateur);
+                    importedFilm.setRealisateur(realisateur);
+                    filmDao.save(importedFilm);
+                //personneDao.save(realisateur); l'appel a la fonction précedente declanche déja une sauvegarde
                 }
-                personneDao.save(realisateur);
             }
             System.out.println(job);
         }
-        importedFilm.setRealisateur(realisateur);
-            filmDao.save(importedFilm);
+
+
+
         System.out.println("--------\nRequetes restantes : "+stripBraces(response.getHeaders().get("x-ratelimit-remaining").toString()));
-        reset = reset = secondsBeforeReset(response.getHeaders().get("x-ratelimit-reset").toString());
+        reset = secondsBeforeReset(response.getHeaders().get("x-ratelimit-reset").toString());
         System.out.println("Temps restant avant reset : "+reset+"\n\n");
     }
 }
