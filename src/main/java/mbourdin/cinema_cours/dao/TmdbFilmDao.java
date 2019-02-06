edc424bd406dda241981ae6b8d3c8b6b093cf2aa
mbourdin1983 <bourdin.maurice@gmail.com>
2019-02-06 15:57:05 +0100

    commit du 6/02/19

--- src/main/java/mbourdin/cinema_cours/dao/TmdbFilmDao.java
+++ src/main/java/mbourdin/cinema_cours/dao/TmdbFilmDao.java
@@ -3,5 +3,8 @@ package mbourdin.cinema_cours.dao;
 import mbourdin.cinema_cours.model.TmdbFilm;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TmdbFilmDao extends CrudRepository<TmdbFilm,Long> {
+    List<TmdbFilm> findAllByTitleContainingIgnoreCase(String str);
 }
