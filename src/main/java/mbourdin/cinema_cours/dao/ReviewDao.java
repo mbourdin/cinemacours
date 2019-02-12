e86dd22d7d062f89e7d437d9c5eb8dd0a3aeb81c
mbourdin1983 <bourdin.maurice@gmail.com>
2019-02-12 08:45:31 +0100

    commit du 12/02/2019

--- src/main/java/mbourdin/cinema_cours/dao/ReviewDao.java
+++ src/main/java/mbourdin/cinema_cours/dao/ReviewDao.java
@@ -5,6 +5,7 @@ import mbourdin.cinema_cours.model.Review;
 import mbourdin.cinema_cours.model.Utilisateur;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -12,5 +13,6 @@ public interface ReviewDao extends CrudRepository<Review,Long> {
        Review findByFilmAndUtilisateur(Film film, Utilisateur utilisateur);
        Set<Review> findAllByFilmAndValideIsTrue(Film film);
        Set<Review> findAllByValideIsFalse();
-
+       Boolean existsByFilmAndUtilisateur(Film film,Utilisateur utilisateur);
+       List<Review> findAllByFilmAndValideIsTrueOrderByDateDesc(Film film);
 }
