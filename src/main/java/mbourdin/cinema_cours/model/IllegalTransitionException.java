2482af8f65e9c3daf1506bc0e3fe0da31ff3644e
mbourdin1983 <bourdin.maurice@gmail.com>
2019-02-19 11:55:22 +0100

    commit du 19/02/2019

---
+++ src/main/java/mbourdin/cinema_cours/model/IllegalTransitionException.java
@@ -0,0 +1,7 @@
+package mbourdin.cinema_cours.model;
+
+public class IllegalTransitionException extends Exception {
+    public IllegalTransitionException(String message){
+        super(message);
+    }
+}
