aa51d0b3ebbd211c061efb9a7191b5084f45be5b
mbourdin1983 <bourdin.maurice@gmail.com>
2019-02-21 10:42:50 +0100

    commit du 21/02/2019 matin

--- src/main/java/mbourdin/cinema_cours/web/MainController.java
+++ src/main/java/mbourdin/cinema_cours/web/MainController.java
@@ -12,18 +12,27 @@ import mbourdin.cinema_cours.service.FilmIdImport;
 import mbourdin.cinema_cours.service.FilmStream;
 import mbourdin.cinema_cours.service.ImageManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 
 @Controller
 public class MainController {
 
-
+    @Value("${flagPath}")
+    String flagPath;
 
     @Autowired
     PersonneDao daoPersonne;
@@ -140,5 +149,21 @@ public class MainController {
     @GetMapping("/error/{id}")
     public String error(@PathVariable Long id)
     {   return "/error/"+id;
+    }
+
+    @RequestMapping(value = "/drapeau/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    void getImage0(HttpServletResponse response, @PathVariable("id") Long id) throws IOException, URISyntaxException {
+
+
+
+        BufferedImage image = ImageIO.read(new File(flagPath+"/" + id+".jpg"));
+
+        response.setContentType("image/jpg");
+        OutputStream out;
+
+        out = response.getOutputStream();
+        ImageIO.write(image, "jpg", out);
+
     }
 }
