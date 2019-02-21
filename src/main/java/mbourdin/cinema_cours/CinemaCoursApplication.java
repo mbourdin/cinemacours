07b65b4e04ef45e23e8bab74f6923878f243a9c9
mbourdin1983 <bourdin.maurice@gmail.com>
2019-02-21 12:10:01 +0100

    commit du 21/02/2019 matin

--- src/main/java/mbourdin/cinema_cours/CinemaCoursApplication.java
+++ src/main/java/mbourdin/cinema_cours/CinemaCoursApplication.java
@@ -3,6 +3,8 @@ package mbourdin.cinema_cours;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class CinemaCoursApplication {
 
--- src/test/java/mbourdin/cinema_cours/CinemaCoursApplicationTests.java
+++ src/test/java/mbourdin/cinema_cours/CinemaCoursApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Locale;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
