c61be1f5cb21c5b61dfa9f432aa9bc3a0dbd94de
mbourdin <bourdin.maurice@gmail.com>
2019-03-05 06:49:44 +0100

    rollback erreur

---
+++ src/main/java/mbourdin/cinema_cours/service/Email.java
@@ -0,0 +1,49 @@
+package mbourdin.cinema_cours.service;
+
+
+
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class Email {
+    private static MailService mailService=new MailService();
+    private static boolean inited=false;
+
+
+
+    public static final String recoveryEmailTexte="Vous avez demandé la recupération de votre mot de passe sur le site Les Nanars Sauvages" +
+            "\n si vous n'êtes pas à l'origine de cette demande veuillez ignorer ce message"+
+            "\n pour vous connecter copiez-collez ce texte dans le champ de connexion à usage unique sur la page de connection:\n";
+
+    public static final String validationEmailTexte="Vous venez de vous inscrire sur le site Les Nanars Sauvages"
+            +"\n Pour activer votre compte veuillez cliquer sur le lien ci-dessous \n";
+
+    private String texte;
+    private String sujet;
+    private InternetAddress[] adresses;
+    public Email(String texte,String sujet,InternetAddress[] adresses) {
+
+        this.texte = texte;
+        this.adresses = adresses;
+        this.sujet=sujet;
+        if(!inited) {
+                mailService.init();
+            inited = true;
+        }
+    }
+
+
+            public void send() {
+            MimeMessage message=mailService.createMimeMessage();
+                try{
+                    message.setRecipients(Message.RecipientType.TO, adresses);
+                    message.setText(texte);
+                    message.setSubject(sujet);
+                    mailService.send(message);
+
+                    System.out.println("fin envoi mail");
+                }catch (MessagingException e){System.out.println("echec d'envoi d'emails");};
+                //System.out.println(texte);
+            }
+}
